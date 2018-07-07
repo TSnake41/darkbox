2e84e2e904036978aa4e27512b10df9b37cadc51
TSnake41 <TSnake@Outlook.fr>
2018-07-07 20:59:50 +0200

    Fix some typos in choice.

--- extras/choice/lang/choice_en.h
+++ extras/choice/lang/choice_en.h
@@ -34,7 +34,7 @@
 #define ERROR_NO_KEY "No choice defined, unable to continue.\n"
 
 #define ERROR_UNEXPECTED_VALUE "Unexpected value : "
-#define ERROR_DEFAULT_KEY_NOT_FOUND "Default choice not found in choices set\n"
+#define ERROR_DEFAULT_KEY_NOT_FOUND "Default choice not found in choices set.\n"
 #define ERROR_DEFAULT_KEY_MISSING "Default choice expected.\n"
 #define ERROR_TIMEOUT_MISSING "Timeout expected.\n"
 
--- extras/choice/lang/choice_fr.h
+++ extras/choice/lang/choice_fr.h
@@ -30,10 +30,10 @@
 
 #define DEFAULT_KEYS "ON"
 
-#define ERROR_UNEXPECTED_ARG "Argument inatendu : "
+#define ERROR_UNEXPECTED_ARG "Argument inattendu : "
 #define ERROR_NO_KEY "Aucun choix défini, impossible de continuer.\n"
 
-#define ERROR_UNEXPECTED_VALUE "Valeur inatendue : "
+#define ERROR_UNEXPECTED_VALUE "Valeur inattendue : "
 #define ERROR_DEFAULT_KEY_NOT_FOUND "Choix par défaut non défini dans les choix possibles.\n"
 #define ERROR_DEFAULT_KEY_MISSING "Choix par défaut attendu.\n"
 #define ERROR_TIMEOUT_MISSING "Timeout attendu.\n"
